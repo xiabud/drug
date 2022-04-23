39dbd5f0bef78838b39e8d192c08ee7d1ca9f637
xiabud <1259777342@qq.com>
2022-04-23 15:06:21 +0800

    数字类型输入框限制前端输入非数字类型。

--- src/test/java/com/xiecode/drug/CodeGenerator.java
+++ src/test/java/com/xiecode/drug/CodeGenerator.java
@@ -125,7 +125,7 @@ public class CodeGenerator {
         // 写于父类中的公共字段
         //strategy.setSuperEntityColumns("id");
         //strategy.setInclude(scanner("表名，多个英文逗号分割").split(","));
-        strategy.setInclude("insell_drug_info");
+        strategy.setInclude("sell_is_gone");
         strategy.setControllerMappingHyphenStyle(true);
         //strategy.setTablePrefix(pc.getModuleName() + "_");
         mpg.setStrategy(strategy);
